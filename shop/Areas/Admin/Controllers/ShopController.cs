2a04d881c669ec5b5981e14f3760e025f2a8918b
KacperMazurIT <kacper.mazur.it@gmail.com>
2020-09-28 15:22:49 +0200

    Added Sending Image of product.

--- shop/Areas/Admin/Controllers/ShopController.cs
+++ shop/Areas/Admin/Controllers/ShopController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using shop.Models.Data;
 using shop.Models.ViewModels.Shop;
@@ -131,7 +133,7 @@ namespace shop.Areas.Admin.Controllers
             return "Ok";
         }
 
-        // POST: Admin/Shop/AddProduct
+        // GET: Admin/Shop/AddProduct
         [HttpGet]
         public ActionResult AddProduct()
         {
@@ -146,6 +148,148 @@ namespace shop.Areas.Admin.Controllers
             }
 
             return View(model);
+        }
+
+        // POST: Admin/Shop/AddProduct
+        [HttpPost]
+        public ActionResult AddProduct(ProductVM model, HttpPostedFileBase file)
+        {
+            // Sprawdzamy model state
+            if(!ModelState.IsValid)
+            {
+                using (Db db = new Db())
+                {
+                    model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                    return View(model);
+                }
+            }
+
+            // sprawdzenie czy nazwa produktu jest unikalna
+
+            using (Db db = new Db())
+            {
+               if (db.Products.Any(x => x.Name == model.Name))
+               {
+                    model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                    ModelState.AddModelError("","Ta nazwa produktu jest zajęta!");
+                    return View(model);
+               }
+            }
+
+            // deklaracja produkt id
+            int id;
+
+            // dodanie produktu i zapis na bazie
+
+            using (Db db = new Db())
+            {
+                ProductDTO product = new ProductDTO();
+
+                product.Name = model.Name;
+                product.Slug = model.Name.Replace(" ", "-").ToLower();
+                product.Description = model.Description;
+                product.Price = model.Price;
+                product.CategoryId = model.CategoryId;
+
+                CategoryDTO catDTO = db.Categories.FirstOrDefault(x => x.Id == model.CategoryId);
+
+                product.CategoryName = catDTO.Name;
+
+                db.Products.Add(product);
+
+                db.SaveChanges();
+
+                // pobranie id dodanego produktu
+                id = product.Id;
+
+            }
+
+            //ustawiamy komunikat
+            TempData["SM"] = "Dodałeś produkt";
+
+            #region Upload Image
+
+            // Utworzenie potrzebnej struktury katalogów 
+            var orginalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+
+            var pathString1 = Path.Combine(orginalDirectory.ToString(), "Products");
+            var pathString2 = Path.Combine(orginalDirectory.ToString(), "Products\\" + id.ToString() );
+            var pathString3 = Path.Combine(orginalDirectory.ToString(), "Products\\" + id.ToString() + "\\Thumbs" );
+            var pathString4 = Path.Combine(orginalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery" );
+            var pathString5 = Path.Combine(orginalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery\\Thumbs" );
+
+            if (!Directory.Exists(pathString1))
+            {
+                Directory.CreateDirectory(pathString1);
+            }
+
+            if (!Directory.Exists(pathString2))
+            {
+                Directory.CreateDirectory(pathString2);
+            }
+
+            if (!Directory.Exists(pathString3))
+            {
+                Directory.CreateDirectory(pathString3);
+            }
+
+            if (!Directory.Exists(pathString4))
+            {
+                Directory.CreateDirectory(pathString4);
+            }
+
+            if (!Directory.Exists(pathString5))
+            {
+                Directory.CreateDirectory(pathString5);
+            }
+
+            if(file != null && file.ContentLength > 0)
+            {
+                // sprawdzenie rozszerzenia pliku czy mamy doczynienia z obrazkiem
+                string ext = file.ContentType.ToLower();
+                if (ext != "image/jpg" &&  
+                    ext != "image/jpeg" && 
+                    ext != "image/pjpeg"  && 
+                    ext != "image/gif" &&
+                    ext != "image/x-png" &&
+                    ext != "image/png"
+                    )
+                {
+                    using (Db db = new Db())
+                    {
+                        model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                        ModelState.AddModelError("", "Obraz nie został przesłany - nieprawidłowe rozszerzenie obrazu.");
+                        return View(model);
+                    }           
+                }
+
+                // inicjalizacja nazwy obrazka
+                string imageName = file.FileName;
+
+                // zapis nazwy obrazka do bazy 
+                using (Db db = new Db())
+                {
+                    ProductDTO dto = db.Products.Find(id);
+                    dto.ImageName = imageName;
+                    db.SaveChanges();
+                }
+
+                var path1 = string.Format("{0}\\{1}", pathString2, imageName);
+                var path2 = string.Format("{0}\\{1}", pathString3, imageName);
+
+                // zapisujemy orginalny obrazek
+                file.SaveAs(path1);
+
+                // zapisujemy miniaturkę
+                WebImage img = new WebImage(file.InputStream);
+                img.Resize(200, 200);
+                img.Save(path2);
+
+            }
+
+            #endregion Upload Image
+
+            return RedirectToAction("AddProduct");
         }
     }
 }
