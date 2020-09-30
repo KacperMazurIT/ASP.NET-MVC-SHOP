c44716d4faa40aebf03f8ffd706ebb22935a2b42
KacperMazurIT <kacper.mazur.it@gmail.com>
2020-09-30 15:32:57 +0200

    Added Main View and Routes for Client.

--- shop/Areas/Admin/Controllers/ShopController.cs
+++ shop/Areas/Admin/Controllers/ShopController.cs
@@ -501,5 +501,61 @@ namespace shop.Areas.Admin.Controllers
             return RedirectToAction("Products");
         }
 
+        // POST: Admin/Shop/SaveGalleryImages/id
+        [HttpPost]
+        public ActionResult SaveGalleryImages(int id)
+        {
+            // petla po obrazkach
+            foreach (string fileName in Request.Files)
+            {
+                // inicjalizacja
+                HttpPostedFileBase file = Request.Files[fileName];
+
+                // sprawdzenie czy mamy plik i czy nie jest pusty
+                if (file != null && file.ContentLength > 0)
+                {
+                    // ustawiamy sciezki do katalogow
+                    var orginalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+
+                    string pathString1 = Path.Combine(orginalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery");
+                    string pathString2 = Path.Combine(orginalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery\\Thumbs");
+
+                    var path = string.Format("{0}\\{1}", pathString1, file.FileName);
+                    var path2 = string.Format("{0}\\{1}", pathString2, file.FileName);
+
+                    //Zapis obrazków i minaturek
+                    file.SaveAs(path);
+                    WebImage img = new WebImage(file.InputStream);
+                    img.Resize(200,200);
+                    img.Save(path2);
+
+
+                }
+
+            }
+
+            return View();
+        }
+
+        // POST: Admin/Shop/DeleteImage/id
+        [HttpPost]
+        public void DeleteImage(int id, string imageName)
+        {
+            string fullPath1 = Request.MapPath("~/Images/Uploads/Products/" + id.ToString() + "/Gallery/" + imageName);
+            string fullPath2 = Request.MapPath("~/Images/Uploads/Products/" + id.ToString() + "/Gallery/Thumbs/" + imageName);
+
+            if (System.IO.File.Exists(fullPath1))
+            {
+                System.IO.File.Delete(fullPath1);
+            }
+
+            if (System.IO.File.Exists(fullPath2))
+            {
+                System.IO.File.Delete(fullPath2);
+            }
+        }
+
+        
+
     }
 }
---
+++ shop/Controllers/PagesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using shop.Models.Data;
+using shop.Models.ViewModels.Pages;
+
+namespace shop.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{pages}
+        public ActionResult Index(string page = "")
+        {
+            // ustawiamy adres naszej strony
+            if(page == "")
+            {
+                page = "home";
+            }
+
+            // deklarujemy pageVM i pageDTO
+            PageVM model;
+            PageDTO dto;
+
+
+            // Sprawdzamy czy strona istnieje 
+            using (Db db = new Db())
+            {
+                if(!db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new { page = "" });
+                }
+            }
+
+            // pobieramy pageDTO
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            // ustawiamy tytuł naszej strony
+            ViewBag.PageTitle = dto.Title;
+
+            // sprawdzamy czy strona ma pasek boczny
+            if (dto.HasSidebar == true)
+            {
+                ViewBag.Sidebar = "Tak";
+            }
+            else
+            {
+                ViewBag.Sidebar = "Nie";
+            }
+
+            // Inicjalizujemy pageVM
+            model = new PageVM(dto);
+
+            // Zwramay widok z pageVM
+            return View(model);
+        }
+    }
+}
