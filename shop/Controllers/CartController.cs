0deee6db52ea14e900ce753173740022235ae5d9
KacperMazurIT <kacper.mazur.it@gmail.com>
2020-10-05 20:40:13 +0200

    Added Incrementation of products.

--- shop/Controllers/CartController.cs
+++ shop/Controllers/CartController.cs
@@ -125,6 +125,22 @@ namespace shop.Controllers
             return PartialView(model);
         }
 
+        public JsonResult IncrementProduct(int productId)
+        {
+            // Inicjalizacja listy CartVM
 
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            // pobieramy CartVM
+            CartVM model = cart.FirstOrDefault(x => x.ProductId == productId);
+
+            // zwiekszamy ilosc produktu
+            model.Quantity++;
+
+            // przygotowanie danych do JSONA
+            var result = new { qty = model.Quantity, price = model.Price };
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
     }
 }
